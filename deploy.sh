4c292ddf70ab2f6cf2eda672920fe76b31dbcbcd
santechx <hello@santechx.com>
2022-11-22 18:43:57 -0500

    cdk fixes

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 cd infra
 npm install
 npx cdk bootstrap \
-    --profile 046097630868_AdministratorAccess \
+    --profile default \
     --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess \
     aws://046097630868/us-east-1
 npx cdk deploy
